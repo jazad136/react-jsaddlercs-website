d3d831e7f90fdf35a0f3959aa394dda7495f9331
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2021-12-16 16:00:57 -0500

    Last splashy design 12-16

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 import './App.css';
+import SelfPic from './images/homepage-img/SelfPicHome_Reduced.jpg';
 class App extends Component { 
   render() { 
     return (
       <div>
-      <div className="nav-container">
+      <div className="navBar">
           <div className="a-container">
               <div className="a">Home</div>
               <div className="a2">Projects</div>
@@ -14,7 +15,31 @@ class App extends Component {
       <div className="focus-content">
         <div className="a-space">a-space</div>
         <div className="b-container">
-          
+        <div class="b">
+            <div className="homepage-img-box">
+              <img alt="Jonathan A. Saddler" className="homepage-img" src={SelfPic}/>
+            </div>
+                <div className="b2-2">b2-2 (name)</div>
+            </div>
+            <div className="b2">
+              <div className="nameplate">
+                <h2>Jonathan A. Saddler, Ph. D.</h2>
+              </div>
+              <div className="logos">
+                Logo A
+              </div>
+            </div>
+        </div>
+        <div class="b-space">b-space</div>
+        <div class="c-container">
+            <div class="c">
+              ResLinks
+            </div>
+        </div>
+        <div class="d-container">
+            <div class="d">
+              Copyright 2021
+            </div>
         </div>
       </div>
       </div>
---
+++ src/Nameplate.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Nameplate = () => {
+    return (
+        <div className="nameplate">
+            <h2>Jonathan A. Saddler, Ph. D.</h2>
+        </div>
+    )
+}
+
+export default Nameplate;
