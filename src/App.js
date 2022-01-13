70779c2f00a0fd83a862a44ed95342c5eb0de112
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2022-01-13 18:15:14 -0500

    Remove unwanted create-react-app files

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import {BrowserRouter as Router} from 'react-router-dom';
 // images and navbars - temporarily disabled. 
 // eslint-disable-next-line
 import SelfPic from './images/homepage-img/SelfPicHome_Reduced.jpg';
-// eslint-disable-next-line
-import Navbar from './Navbar'
+//import Navbar from './Navbar'
 
 class App extends Component { 
   
@@ -21,25 +20,5 @@ class App extends Component {
     );
   }
 }
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
