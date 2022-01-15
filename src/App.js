1be4d94eb43a664c7c83640d672bbc89a9abbd88
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2022-01-14 20:04:14 -0500

    Add semantic-ui-react

--- src/App.js
+++ src/App.js
@@ -8,13 +8,15 @@ import SelfPic from './images/homepage-img/SelfPicHome_Reduced.jpg';
 //import Navbar from './Navbar'
 
 class App extends Component { 
-  
+  // thanks goes out to https://www.section.io/engineering-education/transitioning-from-bootstrap-to-semantic-ui-react/
+  // for a tutorial on transitioning from bootstrap to react
   render() { 
     return (
       <Router>
         Hi there.
         <div className="container focus-content">
-          This is where content goes.
+          {/* This is where content goes. */}
+          
         </div>
       </Router>
     );
