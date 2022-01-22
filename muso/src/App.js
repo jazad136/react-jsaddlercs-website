b87bbfa4794c4de3a5f4f14253ea81256997dc21
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-01-22 11:09:19 -0500

    FieldsAttempt1

--- muso/src/App.js
+++ muso/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import 'semantic-ui-css/semantic.min.css'
 import './css/App.css';
+import {Grid, Segment} from 'semantic-ui-react';
 import WebFont from 'webfontloader';
 
 WebFont.load({
@@ -13,7 +14,39 @@ WebFont.load({
 export class App extends Component {
   render() {
     return (
-        <div style={{fontFamily: "Oswald"}}>Hi there.</div>
+      <div className="focus-content">
+        This is where content goes. 
+        <Grid columns={2}>
+          <Grid.Column width={2}>
+            <Segment className="a">a</Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Grid columns={3}>
+              <Grid.Column>
+                <Segment className="b">
+                  b
+                  <Grid columns={1}>
+                    <Grid.Column><Segment className="b1-1">b1-1</Segment></Grid.Column>
+                    <Grid.Column><Segment className="b1-2">b1-2</Segment></Grid.Column>
+                    <Grid.Column><Segment className="b2-1">b2-1</Segment></Grid.Column>
+                    <Grid.Column><Segment className="b2-2">b2-2</Segment></Grid.Column>
+                    <Grid.Column><Segment className="b3-1">b3-1</Segment></Grid.Column>
+                    <Grid.Column><Segment className="b3-2">b3-2</Segment></Grid.Column>
+                  </Grid>
+                </Segment>
+              </Grid.Column>
+              <Grid.Column>
+                <Segment className="c">
+                  c
+                  <Grid columns={1}>
+                    
+                  </Grid>
+                </Segment>
+              </Grid.Column>
+            </Grid>
+          </Grid.Column>
+        </Grid>
+      </div>
     );
   }
 }
---
+++ muso/src/components/LineFieldSet.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class LineFieldSet extends Component {
+  render() {
+    const {lineNum} = this.props;
+    return (
+    <div>
+        
+    </div>
+    );
+  }
+}
+
+export default LineFieldSet;
