ace91c5481a540607ee957bf6d015b34e5bca562
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-01-27 20:11:07 -0500

    Change Splash Text and Splash Image

--- node-website/src/components/NameplateSplash.js
+++ node-website/src/components/NameplateSplash.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {Segment, Grid} from 'semantic-ui-react';
-import selfSplash from '../images/homepage-img/FullCaricatureWavy.png' 
+import selfSplash from '../images/homepage-img/FullCaricatureFlat.png' 
 import '../css/NameplateSplash.css'
 export class NameplateSplash extends Component {
   render() {
@@ -8,32 +8,43 @@ export class NameplateSplash extends Component {
       <Grid className="f-g">
         <Grid.Row>
           <Grid.Column>
-            <Segment className="nameplate"><h2>Jonathan A. Saddler, Ph. D.</h2></Segment>
+            <Segment basic className="nameplate">
+              <h1>Jonathan A. Saddler, Ph. D.</h1>
+            </Segment>
           </Grid.Column>
         </Grid.Row>
         <Grid.Row>
           <Grid.Column>
-            <Segment className="g">
-                <Grid columns={2}>
-                  {/* <Grid.Column>
+            <Segment className='splash'>
+              <Grid columns={2}>
+                {/* <Grid.Column>
                     <div class="g1">The Splash</div>
                   </Grid.Column>
                   <Grid.Column textAlign="right">
                     <img alt="PicHead" style={{width : '100px'}} src={selfSplash}/>
                   </Grid.Column> */}
-                  
-                    <div className="splash-txt">
-                        Determined software developer with a desire
-                        to publish interactive websites using the latest
-                        technologies. Also, a Ph. D. researcher with an research interest in analyzing behaviors of software engineers. 
-                        {/* <h3>A Ph. D. Researcher</h3><br/>
+
+                <div className="splash-txt">
+                  <p>
+                    Determined software developer with a desire to publish
+                  interactive websites using the latest technologies. 
+                  </p>
+                  <p>
+                    A Ph. D. researcher interested in analyzing
+                    behaviors of software engineers.
+                  </p>
+                  {/* <h3>A Ph. D. Researcher</h3><br/>
                         <h3>A Gap Jumper</h3><br/>
                         <h3>A Developer</h3> */}
-                    </div>
-                    <div className="splash-pic">
-                      <img alt="PicHead" style={{width : '100px'}} src={selfSplash}/>
-                    </div>
-                </Grid>
+                </div>
+                <div className="splash-pic">
+                  <img
+                    alt="PicHead"
+                    style={{ width: "100px" }}
+                    src={selfSplash}
+                  />
+                </div>
+              </Grid>
             </Segment>
           </Grid.Column>
         </Grid.Row>
