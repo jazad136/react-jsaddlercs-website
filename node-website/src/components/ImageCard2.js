4b0c5633826cc00140be5f4fe51c3979e8bb43a9
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-02-03 12:12:09 -0500

    Add New CV

--- node-website/src/components/ImageCard2.js
+++ node-website/src/components/ImageCard2.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Grid, Image, Button, Segment} from 'semantic-ui-react';
 import selfPic from "../images/homepage-img/SelfPicLin_cr.jpg"
-import cvPdf from '../images/homepage-img/JSaddler_CV.pdf';
+import cvPdf from '../images/homepage-img/JSaddler_CV_Web.pdf';
 import resumePdf from '../images/homepage-img/JS_Resume_Web.pdf';
 import "../css/ImageCard.css";
 export class ImageCard2 extends Component {
