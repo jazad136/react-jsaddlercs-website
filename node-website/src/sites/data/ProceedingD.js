35288a2d69b996fa2201c3768d75e39512ab9e28
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-02-23 08:30:39 -0500

    Import Data Tables Feature into Main Website

--- node-website/src/sites/data/ProceedingD.js
+++ node-website/src/sites/data/ProceedingD.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types'
 import {List, Segment, Icon} from 'semantic-ui-react'
 export const ProceedingD = (props) => {
   
-  
   const getAvailableLink = (paperDoi) => { 
     if(paperDoi) { 
       const fullLink = `https://doi.org/${paperDoi}`
@@ -29,20 +28,44 @@ export const ProceedingD = (props) => {
       return ""
     }
   }
+  const getTablesLinks = (tables) => {
+    if(tables === undefined || tables.length === 0) { 
+      return null; 
+    } 
+    return (
+      <Fragment>
+      <ul className='paper-tables'>
+        {tables.map((table, index) => (
+          <List.Item as='li' key={index}>
+            <div>
+              <Icon name='table' size='small'/>
+            </div>
+            <div>
+              <a href={table.importName}>
+                {table.displayName}
+              </a>
+            </div>
+          </List.Item>
+        ))}
+      </ul>
+    </Fragment>) 
+  }
 
   return (
     <List.Item>
       <Segment>
         {props.children}<br/>
         {getPreprintLink(props.preprint)}&nbsp;
-        {getAvailableLink(props.paperDoi)}
+        {getAvailableLink(props.paperDoi)}&nbsp;
+        {getTablesLinks(props.csvTables)}
       </Segment>
     </List.Item>
   );
 };
 ProceedingD.propTypes = {
   preprint: PropTypes.string,
-  paperDoi: PropTypes.string
+  paperDoi: PropTypes.string,
+  cavTables: PropTypes.array
 }
 export default ProceedingD;
 
--- node-website/src/sites/data/ProceedingsD.js
+++ node-website/src/sites/data/ProceedingsD.js
@@ -2,12 +2,16 @@ import React,{Fragment} from 'react';
 import '../../css/proceeding-block.css'
 import efstool_pdf from "../../images/paper-img/Saddler_EFSTool_preprint.pdf";
 import ProceedingD from './ProceedingD'
+import efstool_tab2 from '../../tablescsv/SaddlerEFSGtable2.csv'
+import efstool_tab4 from '../../tablescsv/SaddlerEFSGtable4.csv'
 export const ProceedingsD = () => {
   return (
     <Fragment>
         <h3>Conference Proceedings</h3>
         <ol>
-          <ProceedingD paperDoi="10.1109/SANER48275.2020.9054848">
+          <ProceedingD 
+          paperDoi="10.1109/SANER48275.2020.9054848"
+          >
             <span className="me">Saddler, Jonathan A.</span>, Peterson, Cole S., Sama, Sanjana, 
               Nagaraj, Shruthi, Baysal, Olga, Guerrouj, Latifa, and Sharif, Bonita.
               &quot;Studying Developer Reading Behavior on Stack Overflow 
@@ -17,14 +21,18 @@ export const ProceedingsD = () => {
               Evolution, and Reenginering (SANER 2020).</span>&nbsp;
               London, Ontario, Canada, Feb. 2020. [Online]
           </ProceedingD>
-          <ProceedingD paperDoi="10.1007/978-3-030-22419-6_43">
+          <ProceedingD 
+          paperDoi="10.1007/978-3-030-22419-6_43"
+          >
             <span className="me">Saddler, J. A.</span>, Peterson, C., Peachock, P. and Sharif, B. 
                 &quot;Reading Behavior and Comprehension of C++ Source Code -- A Classroom Study,&quot; 
                 in <span className="paper">Proceedings of the 21st International Conference 
                 on Human Computer Interaction (HCII 2019).</span>&nbsp;
                 Orlando, FL, USA: Springer, July 2019. pp. 597-616. [Online]
           </ProceedingD>
-          <ProceedingD paperDoi="10.1145/3236024.3275534">
+          <ProceedingD 
+          paperDoi="10.1145/3236024.3275534"
+          >
             Mansoor, N., <span className="me">Saddler, J. A.</span>, Silva, B.,  
             Bagheri, H., Cohen, M. B., and Farritor, S. &quot;Modeling and Testing a Family of 
             Surgical Robots: An Experience Report&quot;,  
@@ -33,7 +41,19 @@ export const ProceedingsD = () => {
             of Software Engineering (ESEC/FSE)</span>&nbsp;
             , Nov. 2018. [Online]
           </ProceedingD>
-          <ProceedingD paperDoi="10.1109/ASE.2017.8115711" preprint={efstool_pdf}>
+          <ProceedingD 
+          paperDoi="10.1109/ASE.2017.8115711" 
+          preprint={efstool_pdf}
+          csvTables={[
+              {
+                'displayName': 'Table 2',
+                'importName': efstool_tab2},
+              {
+                'displayName': 'Table 4',
+                'importName': efstool_tab4
+              }
+          ]}
+          >
             <span className="me">Saddler, J. A.</span>, Cohen, M. B. &quot;EventFlowSlicer: A tool for
               generating realistic goal-driven GUI tests,&quot; 
               in <span className="paper">Proceedings of the 32nd IEEE/ACM 
