41a1578f487d14a2d08f26dd097c56fd34d4495e
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-04-10 20:47:25 -0400

    Apr10

--- vactrac/src/App.js
+++ vactrac/src/App.js
@@ -1,29 +1,11 @@
-import React from 'react'
-import SearchBar from './components/SearchBar';
-import VTView from './components/VTView2';
+import React, { Component } from 'react'
 
-export class App extends React.Component {
-  
-  constructor() {
-    WebFont.load({
-        google: {
-            families: [
-              'Oswald: 400, 400italic, 700',
-              'Raleway: 400, 400italic, 700',
-              'Droid Sans: 400, 400italic, 700']
-        }
-    })
-    this.state = {loading: true, rooms : [], hotel1 : null}
-  }
-  
+export class App extends Component {
   render() {
     return (
-      <div className="App">
-        <SearchBar/>
-        <VTView />
-      </div>
+      <div>App</div>
     )
   }
 }
 
-export default App;
+export default App
--- vactrac/src/components/VTView2.js
+++ vactrac/src/components/VTView2.js
@@ -1,10 +1,8 @@
 import React from "react";
 
-export const VTView = (props) => {
+export const VTView2 = (props) => {
   const { id, name, bedInfo, nights, stayStart } = props.hotel;
-
-  return (
-    // hotel contains one hotel.
+// hotel contains one hotel.
     // hotel.id
     // hotel.name
     // hotel.bedInfo
@@ -16,22 +14,24 @@ export const VTView = (props) => {
     // hotel.shortName
     // hotel.address
     // hotel.topAmenities
-    <div className="vt-view">
-      <div className="ui grid two wide">
-        <div className="ui column">
-          <div className="a">
-            <div>Name</div>
-            <div>Address</div>
-            <div>Link</div>
-          </div>
-        </div>
-        <div className="ui column">
-          <div className="b">
-            <div>Chain</div>
-          </div>
-        </div>
-      </div>
-    </div>
+  return (
+    <div>VTView2</div>
+    // <div className="vt-view">
+    //   <div className="ui grid two wide">
+    //     <div className="ui column">
+    //       <div className="a">
+    //         <div>Name</div>
+    //         <div>Address</div>
+    //         <div>Link</div>
+    //       </div>
+    //     </div>
+    //     <div className="ui column">
+    //       <div className="b">
+    //         <div>Chain</div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
-export default VTView;
+export default VTView2;
