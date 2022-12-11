7960ec8e3e45f0144c0c275db00a2b10900d9c11
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-12-11 07:33:08 -0500

    [S10INDEX] jonsdevs_index contains references to wviewer page

--- jonsdevs-index/src/App.js
+++ jonsdevs-index/src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import sunPic from './images-index/sun-solid.svg'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>J. Saddler</h3>
+      <h3>Backends</h3>
+      <ul>
+        <li class="backendsItem">
+          <div>
+            <h3><a href="/weather">Michigan Weather</a></h3>
+          </div>
+          <div>
+            <div className="itemSubContent">
+              <img src={sunPic} alt="weather"/>
+            </div>
+              <h3 className="itemSubContent">Web Page</h3>
+              <h3 className="itemSubContent">API</h3>
+          </div>
+        </li>
+      </ul>
     </div>
-  );
+
+  )
 }
 
 export default App;
